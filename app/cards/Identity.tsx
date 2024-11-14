6f60e9b43d5d1e9f099435c934977b745d78fe35
Adithya Ajith <adithya@Adithyas-Mini.attlocal.net>
2024-11-14 08:12:00 -0600

    Rescale 2xl components

--- app/cards/Identity.tsx
+++ app/cards/Identity.tsx
@@ -7,21 +7,21 @@ export default function Identity() {
   return (
     <Card className="max-w-80 2xl:max-w-2xl">
       <Heading className="bg-accent">
-        <div className="text-xl 2xl:text-6xl p-1 border-b-2 border-foreground 2xl:p-4">
+        <div className="text-xl 2xl:text-5xl p-1 border-b-2 border-foreground 2xl:p-4">
           <h1 className="underline font-bold">Adithya Ajith</h1>
           <h2 className="text-base 2xl:text-2xl">Artist / Engineer</h2>
         </div>
       </Heading>
       <div className="text-xs p-1 flex flex-col h-full items-center justify-evenly ">
         <Avatar />
-        <div className="m-4 text-base 2xl:text-4xl">
+        <div className="m-4 text-base 2xl:text-3xl">
           I am Adithya - a <b>Cybersecurity Masters Student</b> and{" "}
           <b>full-time software engineer</b>. When I&apos;m not working,
           I&apos;m also a multi-media artist, designer, and general tinkerer.
         </div>
         <div className="self-center">
           <Button color="accent">
-            <h1 className="2xl:text-6xl">Résumé</h1>
+            <h1 className="2xl:text-4xl">Résumé</h1>
           </Button>
         </div>
       </div>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,13 +4,13 @@ export default function Navbar() {
   return (
     <div className="w-full h-16 2xl:h-24 flex justify-end items-center border-b-2 border-foreground bg-background">
       <Button color="accent">
-        <h1 className="2xl:text-5xl">Home</h1>
+        <h1 className="2xl:text-3xl">Home</h1>
       </Button>
       <Button color="accent">
-        <h1 className="2xl:text-5xl">Portfolio</h1>
+        <h1 className="2xl:text-3xl">Portfolio</h1>
       </Button>
       <Button color="accent">
-        <h1 className="2xl:text-5xl">Contact</h1>
+        <h1 className="2xl:text-3xl">Contact</h1>
       </Button>
     </div>
   );
